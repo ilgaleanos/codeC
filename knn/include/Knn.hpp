f01b39ec7e8234b10d4c36454841173f646bb1be
ilgaleanos <ilgaleanos@gmail.com>
2016-10-10 11:33:29 -0500

    Normalización de la matriz y adición de comentarios

--- knn/include/Knn.hpp
+++ knn/include/Knn.hpp
@@ -1,77 +1,105 @@
+/*
+*	Objeto implementado con el apoyo de la libreria GSL consultar la documentación
+*	https://www.gnu.org/software/gsl/ para una completa vista aclaratoria.
+*/
+
 #ifndef HAVE_INLINE
 #define  HAVE_INLINE
 #endif
 
-
+/*
+*	(_size, _dim) 		:= dimension de la matrix
+*	gsl_vector_view 	:= vector de vista para normalizar
+*/
 class Knn {
 	private:
 		size_t _size, _dim;
-		double _distancia_tmp;
+		gsl_vector_view _columna;
+		double _distancia_tmp, _norm_tmp;
 		gsl_vector * _vector, * _distancias;
 		gsl_matrix * _matriz;
 	public:
-		Knn(const size_t &size, const size_t &dim);
+		Knn(const size_t &size, const size_t &dim, const bool &ban);
 		inline double get_vector_val(const size_t &i);
 		inline double get_matrix_val(const size_t &i, const size_t &j);
 		inline void set_vector_val(const size_t &i, const double &val);
 		inline void set_matrix_val(const size_t &i, const size_t &j, const double &val);
 
+		void normalizar();
 		void distancia(const size_t &tam);
 
 		virtual ~Knn();
 };
 
-
-Knn::Knn(const size_t &size, const size_t &dim) {
+/*
+*	La bandera ban nos indica si debemos inicializar de manera aleatoria el problema
+*	solicitamos memoria para la matriz, el vector objetivo y de distancias
+*/
+Knn::Knn(const size_t &size, const size_t &dim, const bool &ban) {
 	_size = size;
 	_dim = dim;
 	_vector 	= gsl_vector_alloc (_dim);
 	_distancias = gsl_vector_alloc (_size);
 	_matriz 	= gsl_matrix_alloc (_size, _dim);
 
+
 	// Seed generation based on time
-	struct timeval tv;
-	gettimeofday(&tv,0);
-	unsigned long seed = tv.tv_sec + tv.tv_usec;
+	if (ban == true) {
+		/* code */struct timeval tv;
+		gettimeofday(&tv,0);
+		unsigned long seed = tv.tv_sec + tv.tv_usec;
 
-	gsl_rng_env_setup();
-	const gsl_rng_type * T = gsl_rng_default;
-	gsl_rng * r = gsl_rng_alloc (T);
-	gsl_rng_set(r, seed);
+		gsl_rng_env_setup();
+		const gsl_rng_type * T = gsl_rng_default;
+		gsl_rng * r = gsl_rng_alloc (T);
+		gsl_rng_set(r, seed);
 
-	for (size_t j = 0; j < _dim; j++) {
-		gsl_vector_set(_vector, j, gsl_rng_uniform(r));
-	}
-
-	for (size_t i = 0; i < _size; i++) {
 		for (size_t j = 0; j < _dim; j++) {
-			gsl_matrix_set(_matriz, i, j, gsl_rng_uniform(r));
+			gsl_vector_set(_vector, j, gsl_rng_uniform(r));
+		}
+
+		for (size_t i = 0; i < _size; i++) {
+			for (size_t j = 0; j < _dim; j++) {
+				gsl_matrix_set(_matriz, i, j, gsl_rng_uniform(r));
+			}
 		}
 	}
 
 }
 
-
+/*
+*	getters y setters externos para vector y matriz
+*/
 inline double Knn::get_vector_val(const size_t &i) {
 	return gsl_vector_get(_vector, i);
 }
-
 
 inline void Knn::set_vector_val(const size_t &i, const double &val) {
 	gsl_vector_set(_vector, i, val);
 }
 
-
 inline double Knn::get_matrix_val(const size_t &i, const size_t &j) {
 	return gsl_matrix_get(_matriz, i, j);
 }
-
 
 inline void Knn::set_matrix_val(const size_t &i, const size_t &j, const double &val) {
 	gsl_matrix_set(_matriz, i, j, val);
 }
 
+/*
+*	Normalización de _matriz basados en norma euclidea
+*/
+void Knn::normalizar() {
+	for (size_t j = 0; j < _dim; j++) {
+		_columna = gsl_matrix_column(_matriz, j);
+		_norm_tmp = 1 / gsl_blas_dnrm2(&_columna.vector);
+		gsl_vector_scale(&_columna.vector, _norm_tmp);
+	}
+}
 
+/*
+*	Cálculo de vecindades cercanas basados en distancia euclidea
+*/
 void Knn::distancia(const size_t &tam) {
 	if ( tam >= _size) {
 		fprintf(stderr,"%s\n","ERROR: Búsqueda de vecinos supera dimensión de vecindad");
@@ -102,7 +130,9 @@ void Knn::distancia(const size_t &tam) {
 	free (small);
 }
 
-
+/*
+*	Liberamos memoria
+*/
 Knn::~Knn() {
 	gsl_vector_free (_vector);
 	gsl_vector_free (_distancias);
--- knn/main.cpp
+++ knn/main.cpp
@@ -1,5 +1,12 @@
 /*
-*	@author := ilgaleanos@gmail.com
+*	@author := ilgaleanos@gmail.com*
+*	El siguiente algoritmo es una implementacion simple del algoritmo knn o
+*	k vecindades cercanas, para ello tendremos una matriz (_size,_dim) de datos
+* 	y un vector (1,_dim) los cuales se pueden o no inicializar de manera pseudoaletoria.
+*	el objetivo es encontrar con cuales "filas" de la matriz está mas "cerca" el
+*	vector dado, aquí usamos la distancia euclideana. La función retorna los k
+*	índices de las filas mas cercanas a nuestro vector. No se ha realizado aún la
+*	implementación de lectura de datos desde disco.
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,6 +15,7 @@
 #include <math.h>
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_vector.h>
+#include <gsl/gsl_blas.h>
 #include <gsl/gsl_rng.h>
 #include <gsl/gsl_sort_vector_double.h>
 #include <sys/time.h>
@@ -17,7 +25,8 @@ using namespace std;
 
 
 int main (void) {
-	Knn * esp = new Knn(20,2);
+	Knn * esp = new Knn(20,2,true);
+	esp->normalizar();
 	esp->distancia(10);
 	delete esp;
 	return 0;
