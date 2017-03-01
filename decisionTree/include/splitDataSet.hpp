d047c1fe031df25d8418e24d0a2d67703322310c
ilgaleanos <ilgaleanos@gmail.com>
2017-02-28 19:57:08 -0500

    funcion de mejor característica para partir datos

--- decisionTree/include/splitDataSet.hpp
+++ decisionTree/include/splitDataSet.hpp
@@ -27,3 +27,52 @@ void splitDataSet(Node **dataset, size_t _size, const size_t axis, const double 
                 }
         }
 }
+
+inline int contain(std::vector<double> &array, const double &value) {
+        for (size_t i = 0; i < array.size(); i++) {
+                if (array[i] == value) {
+                        return i;
+                }
+        }
+        return -1;
+}
+
+
+int chooseBestFeatureToSplit(Node **dataset, size_t _size) {
+        size_t numFeatures = dataset[0]->getNumOfFeatures();
+        double baseEntropy = Entropy(dataset, _size);
+        double bestInfoGain = 0;
+        double newEntropy = 0;
+        double infoGain = 0;
+        int bestFeature = -1;
+
+        std::vector<double> tmp;
+        for (size_t i = 0; i < numFeatures; i++) {
+                tmp.clear();
+
+                for (size_t j = 0; j < _size; j++) {
+                        if ( contain(tmp, dataset[j]->getFeature(i)) != -1) {
+                                tmp.push_back(dataset[j]->getFeature(i));
+                        }
+                }
+
+                newEntropy = 0;
+                for (size_t j = 0, L = tmp.size(); j < L; j++) {
+                        size_t sizeOut = countDataset(dataset, _size, i, tmp[j] );
+                        Node *out[sizeOut];
+                        splitDataSet(dataset, _size, i, tmp[j], out);
+                        newEntropy += ( sizeOut / _size) * Entropy(out, sizeOut);
+
+                        for (size_t i = 0; i < sizeOut; i++) {
+                                delete out[i];
+                        }
+                }
+                infoGain = baseEntropy - newEntropy;
+                if (infoGain > bestInfoGain) {
+                        bestInfoGain = infoGain;
+                        bestFeature = i;
+                }
+        }
+
+        return bestFeature;
+}
--- decisionTree/main.cpp
+++ decisionTree/main.cpp
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <map>
 #include <math.h>
+#include <vector>
 
 #include <iostream>
 
@@ -25,30 +26,37 @@ int main (void) {
         double values[FEAT];
         Node *dataset[SIZE];
 
-        values[0] = 1; values[1] = 1;
+        values[0] = 1;
+        values[1] = 1;
+
         dataset[0] = new Node(values, FEAT, "yes");
         dataset[1] = new Node(values, FEAT, "yes");
-        values[0] = 1; values[1] = 0;
+
+        values[0] = 1;
+        values[1] = 0;
+
         dataset[2] = new Node(values, FEAT, "no");
-        values[0] = 0; values[1] = 1;
+
+        values[0] = 0;
+        values[1] = 1;
+
         dataset[3] = new Node(values, FEAT, "no");
         dataset[4] = new Node(values, FEAT, "no");
 
         printf("%0.16f\n", Entropy(dataset, SIZE));
 
-        Tree *mDemo  = new Tree(*dataset, SIZE);
 
-        double axis = 0;
+
+        size_t axis = chooseBestFeatureToSplit(dataset, SIZE);
         double value = 1;
         size_t sizeOut = countDataset(dataset, SIZE, axis, value );
-
         Node *out[sizeOut];
-
         splitDataSet(dataset, SIZE, axis, value, out);
         for (size_t i = 0; i < sizeOut; i++) {
                 printf("%s\n", out[i]->getLabel().c_str() );
         }
 
+        Tree *mDemo  = new Tree(*dataset, SIZE);
         delete mDemo;
         for (size_t i = 0; i < SIZE; i++) {
                 delete dataset[i];
